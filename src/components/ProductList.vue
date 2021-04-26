4b2f05395aca66b0c95c1ae4d41d7447853c6d28
Engin Demiroğ <engindemirog@gmail.com>
2021-04-26 13:52:54 +0300

    26 Nisan Commit 1

---
+++ src/components/ProductList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <table class="table">
+      <tr v-for="product in products" :key="product.id">
+        <td>{{product.id}}</td>
+        <td>{{product.categoryId}}</td>
+        <td>{{product.name}}</td>
+        <td>{{product.unitPrice}}</td>
+        <td>{{product.unitsInStock}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+
+<script>
+export default {
+   data() {
+     return {
+       products:[
+         {id:1,categoryId:1, name:"Laptop 1",unitPrice:15000, unitsInStock:9},
+         {id:2,categoryId:1, name:"Laptop 2",unitPrice:16000, unitsInStock:9},
+         {id:3,categoryId:2, name:"Laptop 3",unitPrice:15000, unitsInStock:9},
+         {id:4,categoryId:2, name:"Laptop 4",unitPrice:15000, unitsInStock:9},
+         {id:5,categoryId:3, name:"Laptop 5",unitPrice:15000, unitsInStock:9},
+       ]
+     }
+   },
+
+};
+</script>
+
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import ProductList from "../components/ProductList.vue";
 
 Vue.use(VueRouter);
 
@@ -13,18 +14,21 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path:"/products",
+    name:"ProductList",
+    component:ProductList
+  }
+
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes:routes,
 });
 
 export default router;
