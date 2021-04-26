250cdadd6946a766ab5eba7642bd4a7cb7aacd50
Engin Demiroğ <engindemirog@gmail.com>
2021-04-26 15:13:13 +0300

    Commit 4

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,11 +2,11 @@
   <div>
     <table class="table">
       <tr v-for="product in products" :key="product.id">
-        <td>{{product.id}}</td>
-        <td>{{product.categoryId}}</td>
-        <td>{{product.name}}</td>
-        <td>{{product.unitPrice}}</td>
-        <td>{{product.unitsInStock}}</td>
+        <td>{{ product.id }}</td>
+        <td>{{ product.categoryId }}</td>
+        <td>{{ product.name }}</td>
+        <td>{{ product.unitPrice }}</td>
+        <td>{{ product.unitsInStock }}</td>
       </tr>
     </table>
   </div>
@@ -15,27 +15,40 @@
 
 <script>
 export default {
-   data() {
-     return {
-       products:[]
-     }
-   },
-   methods:{
-     async getProducts(){
-       const result = await fetch("http://localhost:3000/products");
-       const resultToProducts = await result.json();
-       this.products = resultToProducts;
-     },
-     async getProductsByCategory(categoryId){
-       const result = await fetch("http://localhost:3000/products?categoryId="+categoryId);
-       const resultToProducts = await result.json();
-       this.products = resultToProducts;
-     }
-   },
-   mounted(){
-     this.getProducts();
-   }
-
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    async getProducts() {
+      const result = await fetch("http://localhost:3000/products");
+      const resultToProducts = await result.json();
+      this.products = resultToProducts;
+    },
+    async getProductsByCategory(categoryId) {
+      const result = await fetch(
+        "http://localhost:3000/products?categoryId=" + categoryId
+      );
+      const resultToProducts = await result.json();
+      this.products = resultToProducts;
+    },
+    loadPage() {
+      if (this.$route.params.id) {
+        this.getProductsByCategory(this.$route.params.id);
+      } else {
+        this.getProducts();
+      }
+    },
+  },
+  mounted() {
+    this.loadPage();
+  },
+  watch: {
+    $route() {
+      this.loadPage();
+    },
+  },
 };
 </script>
 
